f25ef0130634a525f0eab0995cfc4e8fca5dfbe3
JCamacho4 <jorgecamagarc@gmail.com>
2023-02-25 17:11:39 +0100

    login and signUp pages with respective functions

--- sporthubReact/src/components/Login.jsx
+++ sporthubReact/src/components/Login.jsx
@@ -5,24 +5,28 @@ export default function Login() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
+  function loginEvent(event) {
+    // Here we will send the username and password to the backend
+    // and check if the user is registered
+    // If the user is registered, we will redirect to the home page
+    // If the user is not registered, we will display an error message
 
-  function handleChangeUsername(event) {
-    setUsername(event.target.value);
+    alert("Username: " + username + " Password: " + password);
   }
 
 
   return (
     <div className="card w-25">
 
-      <form>
+      <form onSubmit={loginEvent}>
         <h3>Log in</h3>
 
         <div className="mb-3">
           <label>Username</label>
           <input
-            type="email"
+            type="username"
             className="form-control"
-            onChange={handleChangeUsername}
+            onChange={(event) => setUsername(event.target.value)}
           />
         </div>
 
@@ -31,11 +35,15 @@ export default function Login() {
           <input
             type="password"
             className="form-control"
+            onChange={(event) => setPassword(event.target.value)}
           />
         </div>
 
         <div>
-          <button type="login" className="btn btn-primary mb-2">
+          <button
+            type="login" 
+            className="btn btn-primary mb-2"
+          >
             Log in
           </button>
         </div>
--- sporthubReact/src/components/SignUp.jsx
+++ sporthubReact/src/components/SignUp.jsx
@@ -1,54 +1,77 @@
+import React from "react";
+
 export default function SignUp() {
-    return (
-      <div className="card w-25">
-  
-        <form>
-          <h3>Sign Up</h3>
 
-          <div className="mb-3">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-            />
-          </div>
+  const [username, setUsername] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-          <div className="mb-3">
-            <label>Name</label>
-            <input 
-              type="text" 
-              className="form-control" 
-              />
-          </div>
-          
-          <div className="mb-3">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-            />
-          </div>
+  function signUpEvent(event) {
+    // Here we will send the username and password to the backend
+    // and check if the user is registered
+    // If the user is registered, we will display an error message
+    // If the user is not registered, we will register the user and redirect to the home page
 
-          <div className="mb-3">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-            />
-          </div>
-
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary mb-2">
-              Sign Up
-            </button>
-          </div>
-          
-          <p className="forgot-password text-right">
-            Already registered? <a href="/login">Log in here!</a>
-          </p>
-        </form>
-  
-      </div>
-    );
+    alert("Username: " + username + " Name: " + name + " Email: " + email + " Password: " + password);
   }
-  
+
+  return (
+    <div className="card w-25">
+
+      <form onSubmit={signUpEvent}>
+        <h3>Sign Up</h3>
+
+        <div className="mb-3">
+          <label>Username</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+
+        <div className="d-grid">
+          <button
+            type="submit"
+            className="btn btn-primary mb-2"
+          >
+            Sign Up
+          </button>
+        </div>
+
+        <p className="forgot-password text-right">
+          Already registered? <a href="/login">Log in here!</a>
+        </p>
+      </form>
+
+    </div>
+  );
+}
